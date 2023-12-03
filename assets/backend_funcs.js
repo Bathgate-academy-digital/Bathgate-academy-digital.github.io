b73a5a49b9715f427ba953f5c98eafef6e882348
Alex Gorichev <Alex.Gorichev@protonmail.com>
2023-12-03 20:10:17 +0000

    Add user creation to backend funcs and test

--- assets/backend_funcs.js
+++ assets/backend_funcs.js
@@ -1,5 +1,21 @@
 async function getLeaderboard(url) {
-  const response = await fetch(url);
-  const json = await response.json();
-  return json;
+  const response = await fetch(`${url}/api/leaderboard`);
+  return response.json();
+}
+
+async function createUser(name, showClass, url) {
+  const requestBody = `name=${name}&class=${showClass}`;
+  console.log(requestBody);
+  const requestHeaders = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/x-www-form-urlencoded'
+  };
+  const options = {
+    method: "POST",
+    headers: requestHeaders,
+    body: requestBody
+  };
+
+  const response = await fetch(`${url}/api/submit`, options);
+  return response.json();
 }
