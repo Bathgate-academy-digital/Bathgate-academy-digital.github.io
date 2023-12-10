1ad483fb32bd783a29f0890676f13723b448dc86
Alex Gorichev <Alex.Gorichev@protonmail.com>
2023-12-10 19:52:28 +0000

    Merge arrow key (test.html) and canvas (game.html)

--- assets/game.js
+++ assets/game.js
@@ -1,13 +1,33 @@
-const tilesX = 10;
-const tilesY = 10;
+// Constants
+const tilesX = 9;
+const tilesY = 9;
 const tileWidth = 20;
 
+// Variables
+let recordedSequence = [];
+let playerX = 4;
+let playerY = 4;
 
+// Elements
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
+// Canvas initialisation
 canvas.width = tileWidth * tilesX;
 canvas.height = tileWidth * tilesY;
+ctx.strokeStyle = 'White';
+ctx.fillStyle = '#98ff98';
+updateCanvas();
+
+// Function definitions
+
+function updateCanvas() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawGrid();
+  drawPlayer(playerX, playerY);
+
+  ctx.strokeRect(0, 0, canvas.width, canvas.height);
+}
 
 function drawGrid() {
   for (let x = 0; x < tilesX; x++) {
@@ -21,35 +41,49 @@ function drawPlayer(x, y) {
   ctx.fillRect(x * tileWidth, y * tileWidth, tileWidth, tileWidth);
 }
 
-let playerX = 0;
-let playerY = 4;
+function startRecording() {
+  recordedSequence.length = 0;
+  document.addEventListener('keydown', (event) => recordKeyPress(event, recordedSequence));
+}
 
-document.getElementsByTagName('body')[0].onkeydown = (event) => {
-  switch (event.key) {
-    case 'ArrowUp':
+function recordKeyPress(event, recordedSequence) {
+  const key = event.key.toLowerCase();
+  if (['arrowup', 'arrowdown', 'arrowleft', 'arrowright'].includes(key)) {
+    recordedSequence.push(key);
+  }
+}
+
+function movePlayer() {
+  document.removeEventListener('keydown', recordKeyPress);
+
+  let i = 0;
+  const intervalId = setInterval(() => {
+    if (i < recordedSequence.length) {
+      const direction = recordedSequence[i];
+      updatePlayerPosition(direction)
+      i++;
+    } else {
+      clearInterval(intervalId);
+    }
+  }, 500);
+}
+
+function updatePlayerPosition(direction) {
+  switch (direction) {
+    case 'arrowup':
       playerY--;
       break;
-    case 'ArrowDown':
+    case 'arrowdown':
       playerY++;
       break;
-    case 'ArrowLeft':
+    case 'arrowleft':
       playerX--;
       break;
-    case 'ArrowRight':
+    case 'arrowright':
       playerX++;
       break;
   }
   playerX = Math.max(0, Math.min(playerX, tilesX - 1));
   playerY = Math.max(0, Math.min(playerY, tilesY - 1));
-};
-
-ctx.strokeStyle = 'White';
-ctx.fillStyle = '#98ff98';
-setInterval(() => {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  drawGrid();
-  drawPlayer(playerX, playerY);
-
-  ctx.strokeRect(0, 0, canvas.width, canvas.height);
-}, 20)
-
+  updateCanvas();
+}
