71893e46bf8be8eaa993cf53d16cfe35d65ee85f
Alex Gorichev <Alex.Gorichev@protonmail.com>
2023-12-14 23:20:45 +0000

    Add update endpoint to backend test and funcs

--- assets/backend_funcs.js
+++ assets/backend_funcs.js
@@ -19,3 +19,20 @@ async function createUser(name, showClass, url) {
   const response = await fetch(`${url}/api/create`, options);
   return response.json();
 }
+
+async function updateScore(id, score, url) {
+  const requestBody = `id=${id}&score=${score}`;
+  console.log(requestBody);
+  const requestHeaders = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/x-www-form-urlencoded'
+  };
+  const options = {
+    method: "POST",
+    headers: requestHeaders,
+    body: requestBody
+  };
+
+  const response = await fetch(`${url}/api/update`, options);
+  return response.json();
+}
