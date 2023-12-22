142a9056568e90cb440df2c56ef76c4342c16225
OPTIC7409 <79322893+OPTIC7409@users.noreply.github.com>
2023-12-22 19:47:16 +0000

    Game WORKING

--- assets/game.js
+++ assets/game.js
@@ -1,44 +1,85 @@
-// Constants
-const tilesX = 9;
-const tilesY = 9;
-const tileWidth = 80;
-
-// Assets
 const playerImage = new Image();
 playerImage.src = "assets/robot.png";
+const endGoalImage = new Image();
+endGoalImage.src = "Media/end_goal.png";
 
-// Variables
-let recordedSequence = [];
-let playerX = 4;
-let playerY = 4;
-
-// Elements
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+const tileWidth = 80;
+const tilesX = 9;
+const tilesY = 9;
 
-// Canvas initialisation
 canvas.width = tileWidth * tilesX;
 canvas.height = tileWidth * tilesY;
-ctx.strokeStyle = 'White';
-ctx.fillStyle = '#98ff98';
-playerImage.onload = updateCanvas;
 
-// Function definitions
+let recordedSequence = [];
+let playerX = 1;
+let playerY = 1;
+let invalidMoveCounter = 0;
+const endPosition = { x: 7, y: 7 };
+
+const tileImages = [];
+const tileMap = [];
+const grassIndex = 1;
+
+function loadTileImages() {
+  const imageSources = ['Media/bad_grass.png', 'Media/grass.png'];
+  imageSources.push("Media/end_goal.png");
+  
+  imageSources.forEach((source, index) => {
+    let img = new Image();
+    img.src = source;
+    img.onload = () => {
+      tileImages[index] = img;
+      if (tileImages.length === imageSources.length) {
+        updateCanvas();
+      }
+    };
+  });
+}
+
+function drawTiles() {
+  for (let y = 0; y < tilesY; y++) {
+    for (let x = 0; x < tilesX; x++) {
+      let tileIndex = tileMap[y][x];
+      if (x === endPosition.x && y === endPosition.y) {
+        ctx.drawImage(endGoalImage, x * tileWidth, y * tileWidth, tileWidth, tileWidth);
+      } else {
+        let tileImage = tileImages[tileIndex];
+        ctx.drawImage(tileImage, x * tileWidth, y * tileWidth, tileWidth, tileWidth);
+      }
+    }
+  }
+}
+
+function initGame() {
+  const mazeLayout = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 1, 1, 1, 0, 1],
+    [1, 0, 1, 0, 0, 0, 1, 0, 1],
+    [1, 0, 1, 1, 1, 0, 1, 1, 1],
+    [1, 0, 0, 0, 1, 0, 0, 0, 1],
+    [1, 1, 1, 0, 1, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1],
+  ];
+
+  for (let y = 0; y < tilesY; y++) {
+    tileMap[y] = [];
+    for (let x = 0; x < tilesX; x++) {
+      tileMap[y][x] = mazeLayout[y][x];
+    }
+  }
+  
+  loadTileImages();
+}
 
 function updateCanvas() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  drawGrid();
+  drawTiles();
   drawPlayer(playerX, playerY);
-
   ctx.strokeRect(0, 0, canvas.width, canvas.height);
-}
-
-function drawGrid() {
-  for (let x = 0; x < tilesX; x++) {
-    for (let y = 0; y < tilesY; y++) {
-      ctx.strokeRect(x * tileWidth + 1, y * tileWidth + 1, tileWidth, tileWidth);
-    }
-  }
 }
 
 function drawPlayer(x, y) {
@@ -46,11 +87,11 @@ function drawPlayer(x, y) {
 }
 
 function startRecording() {
-  recordedSequence.length = 0;
-  document.addEventListener('keydown', (event) => recordKeyPress(event, recordedSequence));
+  recordedSequence = [];
+  document.addEventListener('keydown', (event) => recordKeyPress(event));
 }
 
-function recordKeyPress(event, recordedSequence) {
+function recordKeyPress(event) {
   const key = event.key.toLowerCase();
   if (['arrowup', 'arrowdown', 'arrowleft', 'arrowright'].includes(key)) {
     recordedSequence.push(key);
@@ -63,32 +104,41 @@ function movePlayer() {
   let i = 0;
   const intervalId = setInterval(() => {
     if (i < recordedSequence.length) {
-      const direction = recordedSequence[i];
-      updatePlayerPosition(direction)
+      const validMove = updatePlayerPosition(recordedSequence[i]);
+      if (!validMove) {
+        invalidMoveCounter++;
+        console.log(`Invalid moves: ${invalidMoveCounter}`);
+      }
       i++;
     } else {
-      recordedSequence = [];
       clearInterval(intervalId);
     }
   }, 500);
 }
 
 function updatePlayerPosition(direction) {
+  let newX = playerX;
+  let newY = playerY;
+  
   switch (direction) {
-    case 'arrowup':
-      playerY--;
-      break;
-    case 'arrowdown':
-      playerY++;
-      break;
-    case 'arrowleft':
-      playerX--;
-      break;
-    case 'arrowright':
-      playerX++;
-      break;
+    case 'arrowup': newY--; break;
+    case 'arrowdown': newY++; break;
+    case 'arrowleft': newX--; break;
+    case 'arrowright': newX++; break;
   }
-  playerX = Math.max(0, Math.min(playerX, tilesX - 1));
-  playerY = Math.max(0, Math.min(playerY, tilesY - 1));
-  updateCanvas();
+  
+  if (newX >= 0 && newY >= 0 && newX < tilesX && newY < tilesY && tileMap[newY][newX] !== grassIndex) {
+    playerX = newX;
+    playerY = newY;
+    updateCanvas();
+  }
+
+  if (playerX === endPosition.x && playerY === endPosition.y) {
+    console.log('Maze completed!');
+    return true;
+  }
+  
+  return false;
 }
+
+playerImage.onload = initGame;
