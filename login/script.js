4c5e7acb8daeb451fa1c810e90794435ed690926
Alex Gorichev <Alex.Gorichev@protonmail.com>
2024-02-02 00:00:24 +0000

    Fix mistake where class was converted to upper

--- login/script.js
+++ login/script.js
@@ -1,6 +1,6 @@
 async function submitUser() {
-  const name = document.getElementById('name').value;
-  const schoolClass = document.getElementById('class').value.toUpperCase();
+  const name = document.getElementById('name').value.trim();
+  const schoolClass = document.getElementById('class').value;
   const response = await createUser(name, schoolClass);
   if (response.id == null) {
     const element = document.getElementById("login-error");
